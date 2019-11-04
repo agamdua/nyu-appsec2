0fb8674f8c1435864ce8604705796c6ff69f30ff
Agam Dua <agamdua@users.noreply.github.com>
2019-11-03 21:57:27 -0500

    Functional tests (#2)
    * Added: functional tests before adding alchemy
    
    * Skipping CSRF while I refactor the user functionality

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ from users import User, users, UserDoesNotExist, UniqueConstraintError
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'testing')
 
+# TODO: remove or restrict to tests!
+app.config['WTF_CSRF_ENABLED'] = False
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
