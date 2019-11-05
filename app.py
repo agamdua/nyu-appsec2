ae6b21c9ad8ade98cf207eefd39213a665bbe391
Agam Dua <agamdua@users.noreply.github.com>
2019-11-05 10:38:07 -0500

    Deleted unneccesary code (#6)

--- app.py
+++ app.py
@@ -38,44 +38,6 @@ test_user = User(username="test", password="test")
 test_user.save()
 
 
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    is_authenticated = db.Column(db.Boolean)
-    is_active = db.Column(db.Boolean)
-
-    is_anonymous = False
-
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-
-    @classmethod
-    def get_by_id(cls, user_id):
-        return cls.query.get(int(user_id))
-
-    def get(self):
-        if self.id is not None:
-            return self.__class__.get_by_id(int(self.id))
-        return self.__class__.query.filter_by(username=self.username)
-
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
-        return self
-
-    def create_session(self):
-        self.is_authenticated = True
-        self.is_active = True
-        self.is_anonymous = False
-
-
-db.create_all()
-test_user = User(username="test", password="test")
-test_user.save()
-
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.get_by_id(user_id)
