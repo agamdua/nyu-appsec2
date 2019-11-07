76451e0bfedde5ab9f1d99df750ee8bbde3360ae
Agam Dua <agam@comfylabs.io>
2019-11-06 21:48:04 -0500

    Added: functional test for spell check history

---
+++ tests/test_history.py
@@ -0,0 +1,34 @@
+import pytest
+
+from pytest_mock import mocker
+
+from app import app
+
+
+@pytest.fixture(scope="module")
+def test_client():
+    app.config["WTF_CSRF_ENABLED"] = False
+    testing_client = app.test_client()
+    yield testing_client
+
+
+def test_history(test_client, mocker):
+    login_response = test_client.post(
+        "/login", data=dict(username="test", password="test",), follow_redirects=True,
+    )
+    assert login_response.status_code == 200
+
+
+    mock_check = mocker.patch('utils.run_spell_check')
+    mock_check.return_value = 'stuff'
+    spell_check_response = test_client.post(
+            "/spell_check", data=dict(inputarea="test data"), follow_redirects=True,
+            )
+    assert spell_check_response.status_code == 200
+    assert b'test data' in spell_check_response.data
+
+
+    history_response = test_client.get('/history')
+    assert history_response.status_code == 200
+    assert b'<p id=numqueries>1</p>' in history_response.data
+    assert b'<li><a href="/history/query1" id="query1">Query #1</a></li>' in history_response.data
